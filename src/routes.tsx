08eca4d3bb771b7d9217cd274e4e3152cc831d90
newmediapilot <marcin@newmediapilot.com>
2025-05-29 18:41:15 -0400

    Create routes.tsx

---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './main.css';
+import {CacheStrategy} from './utils/CacheStrategy';
+import {router} from './router';
+import * as React from 'react';
+
+CacheStrategy.configure({timeout: 5000});
+
+ReactDOM
+    .createRoot(document.getElementById("root"))
+    .render(<RouterProvider router={createBrowserRouter(router)}/>
+);

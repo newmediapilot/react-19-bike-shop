f2bccdb0d5aa752634a298140ff1cbaf3dd11f23
newmediapilot <marcin@newmediapilot.com>
2025-05-29 19:37:04 -0400

    Delete List.ts

--- src/services/List.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Fetches the resource, registers the resource in cache
- */
-export async function fetchList() {
-    const fetchPath = 'http://localhost:3000/list';
-    const result = await fetch(fetchPath);
-    return await result.json();
-}

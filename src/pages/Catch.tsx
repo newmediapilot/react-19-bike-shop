dabf703dbf2c069911dd2ab16eea0c61793cd939
newmediapilot <marcin@newmediapilot.com>
2025-05-30 00:17:52 -0400

    Update Catch.tsx

--- src/pages/Catch.tsx
+++ src/pages/Catch.tsx
@@ -6,7 +6,7 @@ import * as React from 'react';
  */
 function Catch() {
     return (
-        <div class="p-5">
+        <div className="p-5">
             <h1 className="mb-5">There was an error handling the request.</h1>
             <NavLink to="/" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Return Home

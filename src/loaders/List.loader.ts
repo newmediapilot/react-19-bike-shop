ba3ffcf36aaf62aca798ba6e5aefe9eea7731276
newmediapilot <marcin@newmediapilot.com>
2025-05-29 21:59:47 -0400

    Update List.loader.ts

--- src/loaders/List.loader.ts
+++ src/loaders/List.loader.ts
@@ -5,6 +5,5 @@ export default async function listLoader() {
     const fetchPath = 'http://localhost:3000/list';
     const result = await fetch(fetchPath);
     const body = await result.json();
-    console.log('listLoader', body);
     return body;
 }

01d9b623a136fd4f8fc1eabb201950db888f1592
newmediapilot <marcin@newmediapilot.com>
2025-05-29 21:59:44 -0400

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -17,9 +17,7 @@ function Search() {
     return (
         <form className="p-5 pb-0"
               onChange={handleSubmit((data) => console.log(data))}
-              onSubmit={(e) => {
-                  e.preventDefault()
-              }}>
+              onSubmit={(e) => {e.preventDefault()}}>
             <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                 <input placeholder="Search..."
                        className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"

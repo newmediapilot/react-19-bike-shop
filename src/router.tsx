58cd23a5f5abdfa96b864ad6f5f7709ceea092a3
newmediapilot <marcin@newmediapilot.com>
2025-05-30 00:10:42 -0400

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import DefaultLayout from './layouts/DefaultLayout';
 import listLoader from './loaders/List.loader';
 import Index from './pages/Index';
 import * as React from 'react';
+import Catch from './pages/Catch';
 
 /**
  * Starter router with one DefaultLayout
@@ -13,11 +14,20 @@ export const router = [
         children: [
             {
                 index: true,
+                element: <Index/>,
+                errorElement: <Catch/>,
                 loader: listLoader,
-                element: <Index/>
-            }
-            // More Routes
+            },
         ]
     },
-    // More Layouts
+    {
+        path: '*',
+        element: <DefaultLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Catch/>,
+            },
+        ]
+    }
 ];

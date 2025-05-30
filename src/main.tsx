25622826dc060e5879b8cb9cab7d9843c64d0616
newmediapilot <marcin@newmediapilot.com>
2025-05-30 19:11:24 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// @ts-ignore
 import ReactDOM from "react-dom/client";
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './main.css';
@@ -8,7 +7,9 @@ import {Provider} from 'react-redux'
 import {store} from './redux/store';
 
 ReactDOM
-    .createRoot(document.getElementById("root"))
+    .createRoot(
+        document.getElementById("root")
+    )
     .render(
         <Provider store={store}>
             <RouterProvider router={createBrowserRouter(router)}/>

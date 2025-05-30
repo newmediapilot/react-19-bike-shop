d7d8287e1723f7245acf77f02772030f2b2131f3
newmediapilot <marcin@newmediapilot.com>
2025-05-30 05:06:31 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,17 +3,19 @@ import Search from '../components/Search';
 import Listings, {ListData} from '../components/Listings';
 import {useLoaderData, useSearchParams} from "react-router"
 import * as React from 'react';
+import {selectLoaderSlice} from '../redux/List.selector';
+import {useAppSelector} from '../redux/store';
 
 /**
  * Initial "homepage"
  * @constructor
  */
 function Index() {
-    const listData: ListData = useLoaderData();
-    const [searchParams, _] = useSearchParams();
-    const detailParam: string | null =  searchParams.get("detail");
+    const listData: ListData = useAppSelector(selectLoaderSlice('listLoader'));
+    const [searchParams] = useSearchParams();
+    const detailParam: string = searchParams.get("detail");
     // @ts-ignore
-    const detailData = !!listData ? listData.find(item => item.id === Number(searchParams.get("detail"))) : null;
+    const detailData = listData.find(item => item.id === Number(searchParams.get("detail")));
     return (
         <>
             <Search/>

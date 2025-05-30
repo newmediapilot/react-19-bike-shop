3a8824987ebf3099071655804a85351ebb7505ad
newmediapilot <marcin@newmediapilot.com>
2025-05-30 05:06:29 -0400

    Update List.loader.ts

--- src/loaders/List.loader.ts
+++ src/loaders/List.loader.ts
@@ -5,10 +5,11 @@
 import {ListData} from '../components/Listings';
 import {addLoaderSlice, store} from '../redux/store';
 
+// @ts-ignore
 export default async function listLoader(): Promise<ListData> {
     const fetchPath = 'http://localhost:3000/list';
     const result = await fetch(fetchPath);
     const data = await result.json();
-    store.dispatch(addLoaderSlice({data, key: fetchPath}));
+    store.dispatch(addLoaderSlice({data, key: 'listLoader'}));
     return data;
 }

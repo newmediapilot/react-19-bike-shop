15bc91c1d55e18a806a21ca417945cdb12643c17
newmediapilot <marcin@newmediapilot.com>
2025-05-29 22:53:20 -0400

    Update Listings.tsx

--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -1,3 +1,5 @@
+import {NavLink} from 'react-router';
+
 export type ListData = {
     id: number,
     title: string,
@@ -11,7 +13,7 @@ export type ListData = {
  */
 const Listings = function List({list = []}: { list: ListData }) {
     return (
-        <div className="flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border p-5">
+        list.length && <div className="flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white shadow-md rounded-lg bg-clip-border p-5">
             <table className="w-full text-left table-auto min-w-max text-slate-800">
                 <thead>
                 <tr className="text-slate-500 border-b border-slate-300 bg-slate-50">
@@ -38,28 +40,21 @@ const Listings = function List({list = []}: { list: ListData }) {
                 </tr>
                 </thead>
                 <tbody>
-                <tr className="hover:bg-slate-50">
-                    <td className="p-4">
-                        <p className="text-sm font-bold">
-                            0
-                        </p>
-                    </td>
-                    <td className="p-4">
-                        <p className="text-sm">
-                            Test
-                        </p>
-                    </td>
-                    <td className="p-4">
-                        <p className="text-sm">
-                            Test
-                        </p>
-                    </td>
-                    <td className="p-4">
-                        <p className="text-sm">
-                            Details
-                        </p>
-                    </td>
-                </tr>
+                    {
+                        list.map((item, a) =>
+                            <tr key={a} className="hover:bg-slate-50">
+                                {Object.keys(item).map((key, b) =>
+                                    <td key={b} className="p-4">
+                                        <p className="text-sm font-bold">{item[key]}</p>
+                                    </td>
+                                )}
+                                <td className="p-4">
+                                    <p className="text-sm font-bold">
+                                        <NavLink to={`./?v=${a}`}>View Detail</NavLink>
+                                    </p>
+                                </td>
+                            </tr>)
+                    }
                 </tbody>
             </table>
         </div>

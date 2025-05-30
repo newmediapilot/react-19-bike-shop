712a2794c9747aa6eb497fdf0d2de719cbcea280
newmediapilot <marcin@newmediapilot.com>
2025-05-30 03:54:40 -0400

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,7 +4,6 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
 
 const reducers = [{ok: (state = {}) => state}];
-const test = {ok2: (state = {}) => state};
 
 export const store = configureStore({
     reducer: reducers[0],
@@ -14,15 +13,12 @@ export const store = configureStore({
  * Hydrates the root store with a new reducer
  * @param reducer
  */
-export const hydrateReducer = (reducer) => {
+export const installReducer = (reducer) => {
     reducers.push(reducer);
     const payload = {};
     for (let i = 0; i < reducers.length; i++) {
         const key = Object.keys(reducers[i])[0];
         payload[key] = reducers[i][key];
     }
-    console.log('payload', payload);
     store.replaceReducer(combineReducers(payload));
 };
-
-hydrateReducer(test)

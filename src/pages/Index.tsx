3b4b8440a9995039d09a46e668c3143e64da1aca
newmediapilot <marcin@newmediapilot.com>
2025-05-30 05:07:42 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import DetailDialog from '../components/DetailDialog';
 import Search from '../components/Search';
 import Listings, {ListData} from '../components/Listings';
-import {useLoaderData, useSearchParams} from "react-router"
+import {useSearchParams} from "react-router"
 import * as React from 'react';
 import {selectLoaderSlice} from '../redux/List.selector';
 import {useAppSelector} from '../redux/store';

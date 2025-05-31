1a295c4e8e090b6dc2faf12311bf5e032aa7578c
newmediapilot <marcin@newmediapilot.com>
2025-05-31 00:49:26 -0400

    Update deploy-gh-pages.sh

--- script/deploy-gh-pages.sh
+++ script/deploy-gh-pages.sh
@@ -5,9 +5,11 @@ git pull
 npm run build
 
 # Deploy gh-pages
+git push origin --delete gh-pages
 git checkout -b gh-pages
+git push --set-upstream origin gh-pages
 git add -f dist/**/*
-git commit -m '[deploy.sh] commit build to gh-pages'
+git commit -m '[deploy-gh-pages.sh] automated commit build to origin/gh-pages'
 git push
 gh-pages -d dist
 

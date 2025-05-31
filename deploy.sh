56bf4b9995911ede9d541beda052775bb7ee6e99
newmediapilot <marcin@newmediapilot.com>
2025-05-30 23:58:52 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,15 @@ npm run build
 git checkout gh-pages
 
 # Remove non-build files
-#rm -rf ./node_modules
-#rm -rf ./public
-#rm -rf ./.gitignore
-#rm -rf ./deploy.sh
-#rm -rf ./eslint.config.js
-#rm -rf ./index.html
-#rm -rf ./package.json
-#rm -rf ./README.md
-#rm -rf ./vite.config.js
+rm -rf ./node_modules
+rm -rf ./public
+rm -rf ./.gitignore
+rm -rf ./deploy.sh
+rm -rf ./eslint.config.js
+rm -rf ./index.html
+rm -rf ./package.json
+rm -rf ./README.md
+rm -rf ./vite.config.js
+
+# Dist to root folder
+mv dist/* .

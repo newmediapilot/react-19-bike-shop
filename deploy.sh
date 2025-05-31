f6ea53870757f33917842d32e751b26e0483dc3d
newmediapilot <marcin@newmediapilot.com>
2025-05-31 00:11:43 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,25 +9,19 @@ npm run build
 # Checkout gh-pages
 git checkout gh-pages
 
-# Remove non-build files
-rm -rf ./node_modules
-rm -rf ./public
-rm -rf ./.gitignore
-rm -rf ./deploy.sh
-rm -rf ./eslint.config.js
-rm -rf ./index.html
-rm -rf ./vite.config.js
-
 # Dist to root folder
 mv dist/* .
 
 # Deploy gh-pages
+git add .
+git commit -m '[deploy.sh] commit build to gh-pages'
+git push
 npm run deploy
-
-# Reset to original
-git reset --hard
 
 # Move back to main
 git checkout main
 
 echo "Build complete..."
+echo "https://newmediapilot.github.io/react-19-bike-shop"
+echo "https://newmediapilot.github.io/react-19-bike-shop"
+echo "https://newmediapilot.github.io/react-19-bike-shop"

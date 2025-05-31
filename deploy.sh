35f483412bb928c106ab23c53990a1544c9c1852
newmediapilot <marcin@newmediapilot.com>
2025-05-30 23:53:18 -0400

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# Get latest
+git fetch
+git pull
+
+# Build the app
+npm run build
+
+# Checkout gh-pages
+git checkout gh-pages
+
+# Remove non-build files
+#rm -rf ./node_modules
+#rm -rf ./public
+#rm -rf ./.gitignore
+#rm -rf ./deploy.sh
+#rm -rf ./eslint.config.js
+#rm -rf ./index.html
+#rm -rf ./package.json
+#rm -rf ./README.md
+#rm -rf ./vite.config.js
